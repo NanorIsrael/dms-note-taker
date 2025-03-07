e96898ca422ec0e1da3b070639709de85aa9c73e
nanorisrael <israelnanor88@gmail.com>
2025-03-07 22:36:28 +0000

    Add products app

--- dms/settings.py
+++ dms/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'djangoheaders',
+	'rest_framework',
+	'products',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True
