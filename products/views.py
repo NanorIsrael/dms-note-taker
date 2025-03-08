8d8dda179f374372f8e67281f73a0d226baf0726
nanorisrael <israelnanor88@gmail.com>
2025-03-08 18:06:00 +0000

    Add products and user api views

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
+import random
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.views import APIView
 
-from .models import Product
+from .models import Product, User
 from .serializers import ProductSerializer
 
 # Create your views here.
@@ -12,10 +14,31 @@ class ProductViewSet(viewsets.ViewSet):
 		return Response(serializer.data)
 
 	def create(self, request):
-		print('====>', request)
-	def retrieve(self, request):
-		print('====>', request)
-	def update(self, request):
-		print('====>', request)
-	def delete(self, request):
-		print('====>', request)
+		serializer = ProductSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+	def retrieve(self, request, pk=None):
+		products = Product.objects.get(id=pk)
+		serializer = ProductSerializer(products)
+		return Response(serializer.data)
+
+	def update(self, request, pk=None):
+		product = Product.objects.get(id=pk)
+		serializer = ProductSerializer(instance=product, data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+	def destroy(self, request, pk=None):
+		product = Product.objects.get(id=pk);
+		product.delete()
+		return Response({"status": True}, status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(APIView):
+	def get(self, _):
+		users = User.objects.all()
+		user = random.choice(users)
+		return Response({"id": user.id})
