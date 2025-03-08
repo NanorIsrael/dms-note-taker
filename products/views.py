7e6f4b735d1855a004aea67620687b91566b3881
nanorisrael <israelnanor88@gmail.com>
2025-03-08 15:32:16 +0000

    Add serializer and viewset

--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+	def list(self, request):
+		products = Product.objects.all()
+		serializer = ProductSerializer(products, many=True)
+		return Response(serializer.data)
+
+	def create(self, request):
+		print('====>', request)
+	def retrieve(self, request):
+		print('====>', request)
+	def update(self, request):
+		print('====>', request)
+	def delete(self, request):
+		print('====>', request)
